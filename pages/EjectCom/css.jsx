45cd812b00e2d3bbc987774e04105ca474262bbf
unknown <huanglin@supcon.com>
2024-09-20 08:33:35 +0800

    底部菜单

---
+++ pages/EjectCom/css.jsx
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    ejectComMain:{
+        height: 200
+    }
+})
+export default styles;
---
+++ pages/RecommendScreen/index.jsx
@@ -0,0 +1,8 @@
+import { View } from "react-native";
+
+const RecommendScreen=()=>{
+    return(
+        <View>推荐</View>
+    )
+}
+export default RecommendScreen;
