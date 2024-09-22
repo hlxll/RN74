f8ed745e8114bc493766740460cd5215d71e9a72
hlxll <447404195@qq.com>
2024-09-22 19:35:31 +0800

    更新底部，连接svg库

--- App.tsx
+++ App.tsx
@@ -9,6 +9,8 @@ import DynamicScreen from './pages/DynamicScreen';
 import OwnScreen from './pages/OwnScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { Image, StyleSheet } from 'react-native';
+import { SvgUri } from 'react-native-svg';
+const svgUrl = '<?xml version="1.0" standalone="no"?><svg class="icon" width="200px" height="200.00px" viewBox="0 0 1024 1024" version="1.1"><path fill="#000000" d="M512 1024C229.229714 1024 0 794.770286 0 512 0 229.229714 229.229714 0 512 0 794.770286 0 1024 229.229714 1024 512 1024 794.770286 794.770286 1024 512 1024ZM512 80.847238C273.871238 80.847238 80.847238 273.871238 80.847238 512 80.847238 750.128762 273.871238 943.152762 512 943.152762 750.128762 943.152762 943.152762 750.128762 943.152762 512 943.152762 273.871238 750.128762 80.847238 512 80.847238ZM229.059048 794.940952 404.21181 404.21181 794.940952 229.059048 619.78819 619.78819 229.059048 794.940952ZM444.635429 444.635429 323.364571 700.635429 579.364571 579.364571 444.635429 444.635429Z" /></svg>';
 const Tab = createBottomTabNavigator();
 function App() {
   return (
@@ -17,8 +19,9 @@ function App() {
         <Tab.Screen name="推荐" component={RecommendScreen}
           options={{
             tabBarIcon: ({focused})=>(
-              <Image source={focused?require('./static/image/recommend_red.png'):require('./static/image/recomment.png')} 
-              style={focused?styles.bottomIcon:styles.focusedBottomIcon}/>
+              <SvgUri uri={svgUrl} width={20} height={20}/>
+              // <Image source={focused?require('./static/image/recommend_red.png'):require('./static/image/recomment.png')} 
+              // style={focused?styles.bottomIcon:styles.focusedBottomIcon}/>
             ),
           }}/>
         <Tab.Screen name="发现" component={DiscoverScreen} 
