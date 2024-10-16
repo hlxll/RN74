5ede619ba381e7e14ced630b08827951d43b9f7b
hlxll <447404195@qq.com>
2024-10-16 19:25:54 +0800

    开始播放列表

--- src/component/AudioListView/index.jsx
+++ src/component/AudioListView/index.jsx
@@ -1,12 +1,22 @@
 import { View } from 'react-native';
+import React from 'react';
 import styles from './styles';
+import LinearGradient from 'react-native-linear-gradient';
 const AudioListView = () =>{
     return(
         <View style={styles.AudioListView}>
-            <View style={styles.audioListHead}>
+            <LinearGradient
+                colors={['#344094', '#a02843']}
+                start={{x: 0, y: 0}}
+                end={{x: 0, y: 1 }}
+                style={styles.audioHead}>
+                <View style={styles.audioListHead}>
+
+                </View>
+            </LinearGradient>
+            <View style={styles.autoListContent}>
 
             </View>
-            
         </View>
     );
 };
---
+++ src/component/AudioListView/styles.jsx
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native/types";
+
+const styles = StyleSheet.create({
+    AudioListView: {
+        width: '100%',
+        height: '70%',
+        marginTop: '30%',
+    },
+    audioHead: {
+        borderRadius: 20,
+        height: 200,
+        width: '100%',
+    },
+    autoListContent: {
+        marginTop: -20,
+    },
+});
+export default styles;
