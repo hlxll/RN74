c5ecd97e1e1547b2fd208bf9215e59d6f6d1dc11
hlxll <447404195@qq.com>
2024-10-21 20:09:44 +0800

    拖动方法

--- src/component/AudioListView/index.jsx
+++ src/component/AudioListView/index.jsx
@@ -1,37 +1,37 @@
-import { View, Animated, PanResponder, Dimensions } from 'react-native';
-import React, { useRef } from 'react';
+import { View, Animated, PanResponder, Dimensions, Text } from 'react-native';
+import React, { useEffect, useRef } from 'react';
 import styles from './styles';
 import LinearGradient from 'react-native-linear-gradient';
 const AudioListView = () =>{
-    const animatedRef = useRef(new Animated.Value(0)).current;
-    const PanResRef = useRef(
+    const pan = useRef(new Animated.ValueXY()).current;
+
+    const panResponder = useRef(
         PanResponder.create({
-            onMoveShouldSetPanResponder: () => true,
-            onPanResponderGrant: () => {
-                animatedRef.setValue(0);
-            },
-            onPanResponderMove: Animated.event(
-                [
-                    null,
-                    {//映射数据
-                        dy: animatedRef,
-                    },
-                ],
-                {
-                    useNativeDriver: false,
-                }
-            ),
-            onPanResponderRelease: () => {
-                // animatedRef.flattenOffset();
-            },
+        onMoveShouldSetPanResponder: () => true,
+        onPanResponderGrant: () => {
+            pan.setOffset({
+                x: pan.x._value,
+                y: pan.y._value
+            });
+        },
+        onPanResponderMove: Animated.event(
+            [
+            null,
+            { dx: pan.x, dy: pan.y }
+            ]
+        ),
+        onPanResponderRelease: () => {
+            pan.flattenOffset();
+        }
         })
     ).current;
+
     const {height, width} = Dimensions.get('window');
     const initAnimated = (event)=>{
         if(event.nativeEvent.layout.width < 100){
             return;
         }
-        Animated.timing(animatedRef, {
+        Animated.timing(pan.y, {
             toValue: -height * 0.7,
             duration: 3000,
             useNativeDriver: true,
@@ -46,9 +46,10 @@ const AudioListView = () =>{
                     {
                         width: width,
                         height: height * 0.7 + 20,
-                        transform: [{translateY: animatedRef}],
+                        transform: [{translateY: pan.y}],
                     },
-                ]}>
+                ]}
+                {...panResponder.panHandlers}>
             <View style={styles.AudioListView}
                 onLayout={initAnimated}>
                     <LinearGradient
@@ -57,7 +58,18 @@ const AudioListView = () =>{
                         end={{x: 0, y: 1 }}
                         style={styles.audioHead}>
                         <View style={styles.audioListHead}>
-
+                            <View style={styles.leftText}>
+                                <Text style={styles.leftTextOne}>
+                                    开启私人DJ，感受陪伴听歌
+                                </Text>
+                                <Text style={styles.leftTextTwo}>
+                                    沉浸式感受雨天氛围感</Text>
+                            </View>
+                            <View style={styles.startAudio}>
+                                <Text style={styles.startAudioText}>
+                                    开启
+                                </Text>
+                            </View>
                         </View>
                     </LinearGradient>
                     <View style={[
@@ -66,8 +78,8 @@ const AudioListView = () =>{
                             height: height * 0.7 - 150,
                         },
                     ]}>
-                        <View {...PanResRef.panHandlers}
-                            style={styles.moveIcon}>
+                        <View style={styles.moveIcon}/>
+                            <View style={styles.audioContent}>
 
                         </View>
                     </View>
--- src/component/AudioListView/styles.jsx
+++ src/component/AudioListView/styles.jsx
@@ -13,6 +13,32 @@ const styles = StyleSheet.create({
         height: 200,
         width: '100%',
     },
+    audioListHead: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        height: 160
+    },
+    leftText: {
+        width: '60%',
+    },
+    leftTextOne: {
+        color: '#fff',
+        fontSize: 16,
+    },
+    leftTextTwo: {
+        color: 'rgba(255,255,255,0.8)'
+    },
+    startAudio: {
+        width: 70,
+        height: 40,
+        backgroundColor: 'rgba(255,255,255,0.3)',
+        borderRadius: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    startAudioText: {
+        color: '#fff',
+    },
     autoListContent: {
         marginTop: -40,
         backgroundColor: '#fff',
