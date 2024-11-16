4197bea8c9d76121b37e6854d4ef6f79da00684a
hlxll <447404195@qq.com>
2024-11-16 19:50:28 +0800

    研究相机插件

--- src/pages/EjectCom/index.jsx
+++ src/pages/EjectCom/index.jsx
@@ -1,29 +1,29 @@
 import { Alert, Button, View } from 'react-native';
-import styles from './css.jsx'
-const EjectCom=()=>{
-    const OpenAlert=()=>{
+import styles from './css.jsx';
+const EjectCom = ()=>{
+    const OpenAlert = ()=>{
         Alert.alert(
-            "标题",
-            "alert content",
+            '标题',
+            'alert content',
             [
                 {
                     text: 'Cancel',
-                    onPress: ()=> console.log("close alert"),
-                    style: 'cancel'
+                    onPress: ()=> console.log('close alert'),
+                    style: 'cancel',
                 },
                 {
                     text:'Ok',
-                    onPress:()=>console.log("alert OK")
-                }
+                    onPress:()=>console.log('alert OK'),
+                },
             ]
-        )
-    }
+        );
+    };
     return (
         <View className={styles.ejectComMain}>
             <Button onPress={OpenAlert}>打开alert</Button>
-            
+
         </View>
-    )
-}
+    );
+};
 
 export default EjectCom;
--- src/pages/RecommendScreen/index.jsx
+++ src/pages/RecommendScreen/index.jsx
@@ -5,11 +5,16 @@ import Liebiao from '../../static/image/recommend/liebiao.svg';
 import Saoma from '../../static/image/recommend/saoma.svg';
 import HuaTong from '../../static/image/recommend/huatong.svg';
 import SearchIcon from '../../static/image/recommend/ic_search24px.svg';
-import { RNCamera } from 'react-native-camera';
+import {
+  Camera,
+  useCameraDevice,
+  useCameraPermission,
+} from 'react-native-vision-camera';
 const winWidth = Dimensions.get('window').width;
 const ScrollItem = ({data, footType})=>{
   return(
     <ImageBackground source={data.image}
+      key={data.key}
       style={styles.scrollItem}>
       <View style={styles.scrollItem_head}>
         <HuaTong width={25} height={25} style={styles.scrollItem_head_icon}/>
@@ -19,13 +24,13 @@ const ScrollItem = ({data, footType})=>{
         styles.scrollItem_footer,
         {
           backgroundColor:footType || '#000',
-        }
+        },
       ]}>
         <Text style={[
           {
-            color: footType?'#000':'#fff',
+            color: footType ? '#000' : '#fff',
             paddingLeft: 5,
-          }
+          },
         ]}>
         {data.text}
         </Text>
@@ -34,18 +39,20 @@ const ScrollItem = ({data, footType})=>{
   );
 };
 const App = () => {
+  const {hasPermission, requestPermission} = useCameraPermission();
+  const device = useCameraDevice('back', {
+    physicalDevices: ['wide-angle-camera'],
+ });
+  const camera = useRef();
+  useEffect(() => {
+    if (!hasPermission) {
+        requestPermission();
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [hasPermission]);
   const [dayTime, setDayTime] = useState('11');
   const [scrollData, setScrollData] = useState([]);
   const [scrollNumData, setScrollNumData] = useState([]);
-  const cameraRef = useRef(null);
-  const taskPicture = async ()=>{
-    if(cameraRef.current){
-      console.log(cameraRef.current.takePictureAsync);
-      const options = {quality: 0.5, base64: true};
-      const data = await cameraRef.current.takePictureAsync(options);
-      console.log("shuju=", data);
-    }
-  };
   useEffect(()=>{
     let list = [];
     let i = 0;
@@ -53,6 +60,7 @@ const App = () => {
     let j = 0;
     while(i < 10){
       list.push({
+        key: i,
         title: '每日推荐',
         icon: require('../../static/image/recommend/huatong.svg'),
         image: require('../../static/image/recommend/peopleBack.jpg'),
@@ -60,6 +68,7 @@ const App = () => {
       });
       i++;
       list2.push({
+        key: j,
         title: '14.4万',
         icon: require('../../static/image/recommend/huatong.svg'),
         image: require('../../static/image/recommend/peopleBack.jpg'),
@@ -68,9 +77,15 @@ const App = () => {
       j++;
     }
     setScrollData(list);
-    setScrollNumData(list2)
+    setScrollNumData(list2);
   },[]);
-
+  // 拍照
+  const takePicture = async () => {
+    const photo = await camera.current?.takePhoto({
+      flash: 'auto',
+    });
+    console.log(photo);
+  };
   return (
     <View style={styles.container}>
       <View style={styles.recomm_head}>
@@ -84,13 +99,18 @@ const App = () => {
           style={styles.searchIcon}
           width={20}
           height={20}/>
-          <TextInput placeholder={`❤️失乐隔壁老樊`}
+          <TextInput placeholder={'❤️失乐隔壁老樊'}
             style={styles.input}/>
-          <RNCamera ref={cameraRef}
-            type={RNCamera.Constants.Type.back}
-            flashMode={RNCamera.Constants.FlashMode.on}/>
+            {device != null && hasPermission ? (
+              <Camera
+              ref={camera}
+              device={device}
+              isActive={true}
+              photo={true}
+              />
+            ) : <Text>相机错误</Text>}
           <Saoma style={styles.sao_headIcon}
-          onPress={taskPicture}
+          onPress={takePicture}
           width={20}
           height={20}/>
         </View>
@@ -110,19 +130,19 @@ const App = () => {
             data={scrollData}
             horizontal={true}
             renderItem={({item}) => <ScrollItem data={item}/>}
-            keyExtractor={item => item.id}/>
+            />
         </View>
       </View>
       <View style={styles.scrollList}>
         <View style={styles.scrollHead}>
-          <Text style={styles.scrollHeadTitle}>推荐歌单&nbsp;{`>`}</Text>
+          <Text style={styles.scrollHeadTitle}>推荐歌单&nbsp;{'>'}</Text>
         </View>
         <View>
             <FlatList
             data={scrollNumData}
             horizontal={true}
             renderItem={({item}) => <ScrollItem data={item} footType={'#fff'}/>}
-            keyExtractor={item => item.id}/>
+            />
         </View>
       </View>
     </View>
