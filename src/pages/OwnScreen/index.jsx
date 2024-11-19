438e9a1c57820a85b20922d5d8b42ade4eeaf490
unknown <huanglin@supcon.com>
2024-11-19 09:06:49 +0800

    我的界面修改

--- src/pages/OwnScreen/index.jsx
+++ src/pages/OwnScreen/index.jsx
@@ -1,9 +1,63 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { ImageBackground, Text, View } from 'react-native';
+import HeaderLog from '../../static/image/'
+import styles from './styles';
 const OwnScreen = ()=>{
+    const details = [
+        {
+            num: '3',
+            text:'关注'
+        },
+        {
+            num: '1',
+            text:'粉丝'
+        },
+        {
+            num: 'Lv.8',
+            text:'等级'
+        },
+        {
+            num: '856时',
+            text:'听歌'
+        }
+    ]
     return(
-        <View>
-            <Text>own1</Text>
+        <View style={styles.OwnScreen}>
+            <View style={styles.headLog}>
+                <ImageBackground source={HeaderLog}
+                width={50}
+                height={50}/>
+                <View>
+                    <Text style={styles.nameText}>
+                        二十八200122
+                    </Text>
+                </View>
+                <View>
+                    <View style={styles.textAddRight}>
+                        <Text style={styles.huizhangRight}>
+                            14枚徽章
+                        </Text>
+                        <View style={styles.afterLine}></View>
+                    </View>
+                    <Text style={styles.huizhangRight}>
+                        江西 上饶 · 村龄4年
+                    </Text>
+                </View>
+                <View style={styles.listDetails}>
+                    {
+                        details.map(item=>{
+                            return(
+                                <Text style={styles.detailItem}>
+                                    <Text style={styles.detailItemNum}>
+                                        {item.num}&nbsp;
+                                    </Text>
+                                    {item.text}
+                                </Text>
+                            )
+                        })    
+                    }
+                </View>
+            </View>
         </View>
     );
 };
