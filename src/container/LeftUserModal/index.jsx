c9acaa7af23f8ca3caeb249460c2fdb666c2c1cf
unknown <huanglin@supcon.com>
2024-11-29 10:23:12 +0800

    更新左侧弹窗

--- src/container/LeftUserModal/index.jsx
+++ src/container/LeftUserModal/index.jsx
@@ -5,8 +5,71 @@ import styles from './styles.jsx';
 import LinearGradient from 'react-native-linear-gradient';
 const LeftUserModal = (props)=>{
     const {show} = props;
+    const linkData=[
+        [
+            {
+                icon:'',
+                name:'双人空间',
+                text:'超多人都在玩'
+            }
+        ],
+        [
+            {
+                icon:'',
+                name:'我的消息',
+                num:'37'
+            },
+            {
+                icon:'',
+                name:'云贝中心',
+                text:'兑换黑胶VIP'
+            },
+            {
+                icon:'',
+                name:'装扮中心',
+            },
+            {
+                icon:'',
+                name:'创作者中心',
+            }
+        ],
+        [
+            {
+                icon:'',
+                name:'趣测',
+                text:'点击查看今日运势'
+            },
+            {
+                icon:'',
+                name:'云村有票',
+            },
+            {
+                icon:'',
+                name:'商城',
+            },
+            {
+                icon:'',
+                name:'Beat专区',
+            },
+            {
+                icon:'',
+                name:'云推歌',
+            },
+            {
+                icon:'',
+                name:'彩铃专区',
+                text:'pick你的音乐彩铃'
+            },
+            {
+                icon:'',
+                name:'免流量听歌',
+                text:'最多领24个月会员'
+            }
+        ]
+    ]
     return(
-        <Modal visible={show}>
+        <Modal visible={show}
+        transparent={true}>
             <View style={styles.LeftUserModal}>
                 <View style={styles.head}>
                     <PropleSvg width={20} height={20}/>
@@ -27,7 +90,45 @@ const LeftUserModal = (props)=>{
                             style={styles.progress}/>
                         <Text style={styles.memberNum}>v6</Text>
                     </View>
+                    <Text style={styles.memberText}>
+                        会员福利 | 锦鲤福利上新
+                    </Text>
+                    <View style={styles.head}>
+                        <Text>双11大促! VIP低至4.9折!</Text>
+                        <Saoma style={styles.memberNum}/>
+                    </View>
                 </LinearGradient>
+                {
+                    linkData.map((item, index)=>{
+                        return(
+                            <View key={index} style={styles.linkGroup}>
+                                {
+                                    item.map((link, j)=>(
+                                        <View key={j} style={styles.linkItem}>
+                                            <Text>
+                                                {link.icon}
+                                            </Text>
+                                            <Text style={styles.linkItemName}>
+                                                {link.name}
+                                            </Text>
+                                            <View style={styles.linkRight}>
+                                                {
+                                                    link.text?<Text style={styles.linkRightText}>{link.text}</Text>:''
+                                                }
+                                                {
+                                                    link.num?<View style={styles.linkRightNum}>
+                                                        <Text style={styles.linkRNumText}>{link.num}</Text>
+                                                    </View>:''
+                                                }
+                                                {'>'}
+                                            </View>
+                                        </View>
+                                    ))
+                                }
+                            </View>
+                        )
+                    })
+                }
             </View>
         </Modal>
     );
--- src/container/LeftUserModal/styles.jsx
+++ src/container/LeftUserModal/styles.jsx
@@ -3,6 +3,9 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
     LeftUserModal: {
         padding: 20,
+        width:'90%',
+        marginRight:'10%',
+        backgroundColor:'#fff'
     },
     head: {
         flexDirection:'row',
@@ -14,15 +17,57 @@ const styles = StyleSheet.create({
     },
 
     MemberCenter: {
-        borderRadius: 10,
+        borderRadius: 20,
         overflow:'hidden',
+        height: 100
     },
     memberName: {
         flexDirection:'row',
         flex: 1,
+        alignItems:'center'
     },
     progress: {
         backgroundColor:'#483a39'
+    },
+    memberNum: {
+        marginLeft:'auto'
+    },
+    memberText: {
+        color:'#ddd',
+        marginBottom: 30
+    },
+    linkGroup:{
+        marginBottom: 20,
+        backgroundColor:'#fff',
+        borderRadius: 20,
+        overflow:'hidden'
+    },
+    linkItem:{
+        flexDirection:'row',
+        flex: 1
+    },
+    linkItemName:{
+        fontWeight: '600',
+        fontSize: 16,
+    },
+    linkRight: {
+        marginLeft:'auto',
+        flexDirection:'row',
+        alignItems:'center',
+        color:'#ddd'
+    },
+    linkRightText:{
+        color:'#ddd'
+    },
+    linkRightNum:{
+        width:'auto',
+        textAlign:'center',
+        backgroundColor:'red',
+        borderRadius: 10,
+        height: 20
+    },
+    linkRNumText:{
+        color:'#fff'
     }
 })
 export default styles;
