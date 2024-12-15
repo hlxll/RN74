b41bbe9112dde6602d28d648c5c9d709ab42ee46
hlxll <447404195@qq.com>
2024-12-15 13:43:46 +0800

    开始发现界面

--- src/pages/DiscoverScreen/index.jsx
+++ src/pages/DiscoverScreen/index.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View } from 'react-native';
-
+import Liebiao from '../../static/image/recommend/liebiao.svg';
+import LeftUserModal from '../../container/LeftUserModal';
+import styles from 'style.less'
 const DiscoverScreen = ()=>{
+  const [userModal, setUserModal] = useState(false);
+
     return(
         <View>
+            <Liebiao style={styles.LieheadIcon}
+        width={20}
+        height={20}
+        onPress={()=>{setUserModal(true);}}/>
             <Text>DiscoverScreen</Text>
+            {
+        userModal ?
+      <LeftUserModal show={userModal} setUserModal={setUserModal}/> : ''
+      }
         </View>
     );
 };
