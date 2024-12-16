684b701dfd5f8a297aad500b37888fd86b6f2579
hlxll <447404195@qq.com>
2024-12-16 20:43:56 +0800

    gengxin

--- src/pages/DiscoverScreen/index.jsx
+++ src/pages/DiscoverScreen/index.jsx
@@ -5,18 +5,22 @@ import LeftUserModal from '../../container/LeftUserModal';
 import styles from 'style.less'
 const DiscoverScreen = ()=>{
   const [userModal, setUserModal] = useState(false);
-
     return(
         <View>
             <Liebiao style={styles.LieheadIcon}
-        width={20}
-        height={20}
-        onPress={()=>{setUserModal(true);}}/>
-            <Text>DiscoverScreen</Text>
+            width={20}
+            height={20}
+            onPress={()=>{setUserModal(true);}}/>
+            <View>
+                <View>音乐</View>
+                <View>博客</View>
+                <View>听书</View>
+
+            </View>
             {
-        userModal ?
-      <LeftUserModal show={userModal} setUserModal={setUserModal}/> : ''
-      }
+                userModal ?
+                <LeftUserModal show={userModal} setUserModal={setUserModal}/> : ''
+            }
         </View>
     );
 };
