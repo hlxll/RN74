622bdcbe964602319e7f809f5d5cd78d14cf9db6
hlxll <447404195@qq.com>
2024-12-20 16:43:28 +0800

    更新发现

--- src/pages/DiscoverScreen/index.jsx
+++ src/pages/DiscoverScreen/index.jsx
@@ -2,20 +2,26 @@ import React, { useState } from 'react';
 import { Text, View } from 'react-native';
 import Liebiao from '../../static/image/recommend/liebiao.svg';
 import LeftUserModal from '../../container/LeftUserModal';
-import styles from 'style.less'
+import styles from 'style.js';
 const DiscoverScreen = ()=>{
   const [userModal, setUserModal] = useState(false);
+  const [openTab, setOpenTab] = useState('');
+  const ClickOpen = (type)=>{
+    setOpenTab(type);
+  };
     return(
         <View>
             <Liebiao style={styles.LieheadIcon}
             width={20}
             height={20}
             onPress={()=>{setUserModal(true);}}/>
-            <View>
-                <View>音乐</View>
-                <View>博客</View>
-                <View>听书</View>
-
+            <View style={styles.tabList}>
+                <View style={openTab === 'music' ? styles.openTab : styles.defaultTab }
+                    onPress={()=>{ClickOpen('music');}}>音乐</View>
+                <View style={openTab === 'Boke' ? styles.openTab : styles.defaultTab }
+                onPress={()=>{ClickOpen('Boke');}}>博客</View>
+                <View style={openTab === 'listen' ? styles.openTab : styles.defaultTab }
+                onPress={()=>{ClickOpen('listen');}}>听书</View>
             </View>
             {
                 userModal ?
---
+++ src/pages/DiscoverScreen/style.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    tabList: {
+        flexDirection:'row'
+    },
+    defaultTab: {},
+    openTab: {
+        borderBottom: 1,
+        borderBottomColor: 'red',
+    }
+})
+export default styles;
